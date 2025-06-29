fb8e1fad2ae500c2ef841d9ef873b84926bfa989
elygreen <elycgreen@gmail.com>
2025-06-29 14:42:37 -0700

    Changes

--- SandCrabCave.py
+++ SandCrabCave.py
@@ -10,6 +10,7 @@ def Run():
     # Setup getting to location
     time.sleep(5)
     cf.click_compass()
+    cf.angle_up()
     cf.move_and_click((978, 397), 1, 5)       # entrance > s1
     cf.move_and_click((772, 293), 1, 5)       # s1 > s2
     cf.move_and_click((983, 424), 1, 5)       # s2 > s3
@@ -17,7 +18,8 @@ def Run():
     # First AFK set
     cf.move_and_click((771, 291), 1, 3)       # s4 > afk
     cf.wait_aggro_timer()
-    while True:
+    start_time = time.monotonic()
+    while time.monotonic() < start_time + 5.87 * 60 * 60:
         # Second AFK Set
         cf.move_and_click((753, 135), 1, 5)   # afk > s5
         cf.move_and_click((524, 328), 1, 5)   # s5 > s2
