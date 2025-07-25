8e01a12a4e99f93299c7d11c3efcd724ef3b4020
elygreen <elycgreen@gmail.com>
2025-07-25 15:26:37 -0700

    x

--- coordinates.py
+++ coordinates.py
@@ -22,6 +22,8 @@ tab_settings_zoom = (1414, 361)
 zoom_bar_middle = (1346, 511)
 zoom_bar_min = (1417, 511)
 zoom_bar_max = (1272, 514)
+zoom_bar_1 = (1270, 511)
 zoom_bar_4 = (1389, 513)
+zoom_bar_5 = (1422, 511)
 
 ### SCREEN COORDINATES ###
