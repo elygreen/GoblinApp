9481dec0869daaadef31fd8650bb58e9bfcadcc4
elygreen <elycgreen@gmail.com>
2025-08-20 15:39:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import random
 
 if __name__ == '__main__':
     time.sleep(2)
-    #cf.print_mouse_tk()
-    BrimhavenAgility.run()
+    cf.print_mouse_tk()
+    #BrimhavenAgility.run()
     #NMZ.run()
     #SandCrabCave.Run()
     #ArdyKnights3.run()
