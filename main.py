95200a069fff06d0bb506f99d5d7f5f0653d5f6c
elygreen <elycgreen@gmail.com>
2025-10-17 16:56:27 -0700

    SanCrabCave Randomization
    Added randomization to ore clicking to make it look more humanlike

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import HosidiousCooking
 import KarambwanFishing
 import NMZ
 import SandCrabCave
+import Sandstone
 import common_functions as cf
 
 #TODO: Update NMZ, NMZ.py, SandCrabCave.py
@@ -31,13 +32,14 @@ def auto_alch():
 if __name__ == '__main__':
     time.sleep(2)
     script_total_run_time = script_run_time * 60 * 60
-    #cf.print_mouse_tk(needs_login, script_run_time)
-    #HosidiousCooking.run(needs_login, script_run_time)
-    #GrayChins.run(needs_login, script_run_time)
-    #KarambwanFishing.run(needs_login, script_run_time)
-    #ArdyAgilityCourse.run(needs_login, script_run_time)
-    #BrimhavenAgility.run(needs_login, script_run_time)
-    #NMZ.run(needs_login, script_run_time)
-    #SandCrabCave.Run(needs_login, script_run_time)
-    #ArdyKnightsNoFood.run(needs_login, script_run_time)
-    auto_alch()
+    Sandstone.run(needs_login, script_total_run_time)
+    #cf.print_mouse_tk(needs_login, script_total_run_time)
+    #HosidiousCooking.run(needs_login, script_total_run_time)
+    #GrayChins.run(needs_login, script_total_run_time)
+    #KarambwanFishing.run(needs_login, script_total_run_time)
+    #ArdyAgilityCourse.run(needs_login, script_total_run_time)
+    #BrimhavenAgility.run(needs_login, script_total_run_time)
+    #NMZ.run(needs_login, script_total_run_time)
+    #SandCrabCave.Run(needs_login, script_total_run_time)
+    #ArdyKnightsNoFood.run(needs_login, script_total_run_time)
+    #auto_alch()
